4898f60158fac805dbd22287d219ab4e10b6e923
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-02-16 11:16:34 +0300

    Update Main.js

--- JavaScript/Main.js
+++ JavaScript/Main.js
@@ -1,6 +1,13 @@
 function Calculator_Upload_Display()
 {
+    event.preventDefault();
     var Value;
     var Target = document.getElementById("Calculator_Display");
     Target.innerHTML = Value;
+}
+function Calculator_Shutdown_Display()
+{
+    event.preventDefault();
+    var Target = document.getElementById("Calculator_Display");
+    Target.innerHTML = "&nbsp";
 }
