361de27cdd0a24be8d51ed7bfdaec39abbfff077
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-02-16 11:36:07 +0300

    Add files via upload

--- JavaScript/Calculator_td.js
+++ JavaScript/Calculator_td.js
@@ -2,4 +2,4 @@ var td;
 var Count_Of_Elements_td;
 Li = document.getElementById('Calculator_tr_1');
 Count_Of_Elements_td = td.getElementsByTagName('td').length;
-td.style.setProperty('--Count_Of_Elements_td', Count_Of_Elements_td);
+root.style.setProperty('--Count_Of_Elements_td', Count_Of_Elements_td);
