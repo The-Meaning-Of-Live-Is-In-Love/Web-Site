55498ef3fb0740eb90b40ffe247644a955f8208b
TMOLIIL <themeaningofliveisinlove@gmail.com>
2026-02-16 11:28:56 +0300

    Add files via upload

---
+++ JavaScript/Calculator_td.js
@@ -0,0 +1,5 @@
+var td;
+var Count_Of_Elements_td;
+Li = document.getElementById('Calculator_tr_1');
+Count_Of_Elements_td = td.getElementsByTagName('td').length;
+td.style.setProperty('--Count_Of_Elements_td', Count_Of_Elements_td);
